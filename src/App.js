4b97896d4d80fef82de867fe031553f05752c02f
Tevfik <yazgunoglu1@outlook.com>
2024-01-07 13:22:19 +0300

    added housePage route

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import AboutUs from "./views/AboutUs.js";
 import SignUp from "./views/SignUp.js";
 import SignIn from "./views/SignIn.js";
 import LearnMore from "./views/LearnMore.js";
+import HousePage from './views/housePage.js'; 
 
 // layouts
 import MainLayout from "./layouts/MainLayout.js";
@@ -28,6 +29,7 @@ const router = createBrowserRouter(
       <Route path="signin" element={<SignIn />} />
       <Route path="signup" element={<SignUp />} />
       <Route path="learnmore" element={<LearnMore />} />
+      <Route path="house/:id" element={<HousePage />} /> {/* Use element prop */}
     </Route>
   )
 );
